0a67dcf7f9f76a5a64b7be0b2f816862000421de
assa <ye4h@protonmail.com>
2021-02-17 17:34:21 +0700

    splitting code #no-js

--- CAmS/settings.py
+++ CAmS/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FrontSite'
+    'FrontSite',
+    'UserControl',
+    'ProjectControl',
+    'MainPage',
 ]
 
 MIDDLEWARE = [
@@ -65,12 +66,14 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'form_filters': 'UserControl.templatetags.form_filters',
+            }
         },
     },
 ]
 
 WSGI_APPLICATION = 'CAmS.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
@@ -87,7 +90,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -97,6 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 9
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -105,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -119,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
--- CAmS/urls.py
+++ CAmS/urls.py
@@ -14,12 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from FrontSite import views as main_site
+from MainPage.views import StartPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('<int:id>/', main_site.FrontPage, name="FrontPage"),
     path('', main_site.StartPage, name="StartPage"),
-    re_path(r"logout(\?next=(?P<next>\w+))?$", main_site.Logout, name="logout")
+    re_path(r"logout(\?next=(?P<next>\w+))?$", main_site.Logout, name="logout"),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user/', include('UserControl.urls')),  # Actions with users
+    path('project/', include('ProjectControl.urls')),  # Actions with projects
+    path('', StartPage, name="start_page"),
+    re_path('^action=(?P<action>\w+)$',
+            StartPage,
+            name="start_page_with_action"),
+]
+
+
